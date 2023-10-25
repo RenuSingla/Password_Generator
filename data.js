c47c79bf713189e049ad0d22938879fc4f5caedb
RenuSingla <102131503+RenuSingla@users.noreply.github.com>
2023-10-25 14:45:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,4 @@
+export const numbers = "0123456789";
+export const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+export const special = "!'^+%?/=()_#@{[]}\|;,.<`*:$&";
